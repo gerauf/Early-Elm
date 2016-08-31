eff426ddcc26f4b9e379fadc234fda1767144218
Fergus Orbach <fo4786@gmail.com>
2016-08-31 15:18:28 +0100

    simple counter

--- Main.elm
+++ Main.elm
@@ -1,22 +1,41 @@
-import Html exposing (..)
+import Html exposing (Html, button, div, text)
+import Html.App as Html
+import Html.Events exposing (onClick)
 
+main =
+  Html.beginnerProgram { model = model, view = view, update = update }
 
 -- MODEL
 
-type alias Model = { ... }
+type alias Model = Int
+
+model : Model
+model =
+  0
 
 -- UPDATE
 
-type Msg = Reser | ...
+type Msg = Increment | Decrement | Reset
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
-    Reset -> ...
-    ...
+    Increment ->
+      model + 1
+
+    Decrement ->
+      model - 1
+
+    Reset ->
+      model - model
 
 -- VIEW
 
 view : Model -> Html Msg
 view model =
-  ...
+  div []
+    [ div [] [ text (toString model) ]
+    , button [ onClick Decrement ] [ text "-" ]
+    , button [ onClick Increment ] [ text "+" ]
+    , button [ onClick Reset ] [ text "reset" ]
+    ]
