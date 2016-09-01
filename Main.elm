0a710be56d4068a5ffad348b247fce307606a3ef
Fergus Orbach <fo4786@gmail.com>
2016-09-01 13:42:09 +0100

    random number gen

--- Main.elm
+++ Main.elm
@@ -1,48 +1,36 @@
 import Html exposing (..)
-import Html.App as Html
-import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
-import String
-import Char exposing (..)
+import Html.App as App
+import Html.Events exposing (..)
+import Random
 
 main =
-  Html.beginnerProgram { model = model, view = view, update = update }
+  App.program { init = init, view = view, update = update, subscriptions = subscriptions }
 
 -- MODEL
 
 type alias Model =
-  { name : String
-  , age: String
-  , password : String
-  , passwordAgain: String
+  { dieFace : Int
   }
 
-model : Model
-model =
-  Model "" "" "" ""
+init : (Model, Cmd Msg)
+init =
+  (Model 1, Cmd.none)
+
 
 -- UPDATE
 
 type Msg =
-  Name String
-  | Age String
-  | Password String
-  | PasswordAgain String
+  Roll
+  | NewFace Int
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    Name name ->
-      { model | name = name }
+    Roll ->
+      (model, Random.generate NewFace (Random.int 1 6))
 
-    Age age ->
-      { model | age = age }
-
-    Password password ->
-      { model | password = password }
-
-    PasswordAgain passwordAgain ->
-      { model | passwordAgain = passwordAgain }
+    NewFace newFace ->
+      (Model newFace, Cmd.none)
 
 
 -- VIEW
@@ -50,34 +38,12 @@ update msg model =
 view : Model -> Html Msg
 view model =
   div []
-    [ input [ type' "text", placeholder "Name", onInput Name ] []
-    , input [ type' "number", placeholder "Age", onInput Age ] []
-    , input [ type' "password", placeholder "Password", onInput Password ] []
-    , input [ type' "password", placeholder "Re-enter Password", onInput PasswordAgain ] []
-    , viewValidation model
+    [ h1 [] [ text (toString model.dieFace)]
+    , button [onClick Roll ] [text "Roll"]
     ]
 
-viewValidation : Model -> Html msg
-viewValidation model =
-  let
-    (color, message) =
-      if not ( String.any isDigit model.age ) then
-        ("red", "Age must be a digit")
-      else if model.password /= model.passwordAgain then
-        ("red", "Passwords do not match!")
-      else if String.length model.password < 8 then
-        ("red", "Password too short")
-      else if correctChars model.password then
-        ("red", "The password must contain at least one uppercase letter, one lowercase letter and one number")
-      else
-        ("green", "OK")
-  in
-    div [ style [("color", color)] ] [ text message ]
+-- SUBSCRIPTIONS
 
-correctChars : String -> Bool
-correctChars password =
-  not (
-    String.any isDigit password
-    && String.any isUpper password
-    && String.any isLower password
-    )
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
